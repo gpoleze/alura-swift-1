84a27163b55137f1e94accd9c937bac6697aa4d1
Gabriel Poleze Ferreira <gabriel@jostle.me>
2020-10-04 18:10:06 -0700

    Added the first app's screen

--- eggplant-brownie/ViewController.swift
+++ eggplant-brownie/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var nomeTextField: UITextField!
+    @IBOutlet var nivelFelicidadeTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func adicionar(_ sender: Any) {
+        
+        print("Alimento: \(nomeTextField?.text ?? "") Nível de felicidade \(nivelFelicidadeTextField?.text ?? "")")
+    }
+    
 }
 
